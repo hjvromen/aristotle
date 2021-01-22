35c91734976976d2aaeabae16f402bbd328e8b62
hjvromen <54151485+hjvromen@users.noreply.github.com>
2021-01-22 12:29:13 +0100

    updates 22-01-2021

--- src/aristotle_syntax.lean
+++ src/aristotle_syntax.lean
@@ -12,11 +12,12 @@ inductive copula : Type
 | e : copula
 | o : copula
 
-structure assertoric :=
-mk :: (predicate : term) (x : copula) (subject : term)
+/- variable assertoric : term → copula → term → Prop
+#check assertoric
 
-#check assertoric
-#check assertoric
+variable syllogism : assertoric → assertoric → assertoric → Prop
+
+structure univ_aff extends assertoric := (x = a) -/
 
 constant universal_affirmation (A : term) (B : term) : Prop
 local infixr ` a ` : 80 := universal_affirmation
@@ -30,22 +31,29 @@ local infixr ` i ` : 80 := particular_affirmation
 constant particular_denial (A : term) (B : term) : Prop
 local infixr ` o ` : 80 := particular_denial
 
-/- Aristotle uses these seven axioms / deduction rules (→ Malink)
-   Corcoran calls this system d   
-   System dr is also complete while using only the numbers 1, 2, 5 and 7  (see end of this file for a proof)  -/
+def c : Prop → Prop 
 
--- the 'perfect' syllogisms
+/- Aristotle uses the following seven axioms / deduction rules 
+   and a definition of the contradictory of a proposition
+   see Smith (1989, pp. xix-xx)  -/
+
+-- the four 'perfect' syllogisms
 axiom Barbara₁ : P a M → M a S → P a S
 axiom Celarent₁ : P e M → M a S → P e S
 axiom Darii₁ : P a M → M i S → P i S 
 axiom Ferio₁ : P e M → M i S → P o S
--- the conversion rules
+-- the three conversion rules
 axiom e_conv : A e B → B e A      --   simple conversion
 axiom i_conv : A i B → B i A      --   simple conversion
 axiom a_conv : A a B → B i A      --   accidental conversion
 --  the 'contradictories'
-axiom contr_1 : A a B = ¬ A o B
-axiom contr_2 : A e B = ¬ A i B
+axiom contr_a : c (A a B) = A o B 
+axiom contr_e : c (A e B) = A i B 
+axiom contr_i : c (A i B) = A e B 
+axiom contr_o : c (A o B) = A a B 
+--axiom contr_3 {h1 h2 r : Prop} : h1 → h2 → (c r → c h1) → r 
+--axiom contr_4 {h1 h2 r : Prop} : h1 → h2 → (c r → c h2) → r 
+axiom contr_5 {p r : Prop} : (c r → c p) → p → r
 
 /-
 lemma subaltrn_1: A a B → A i B :=        --   accidental conversion
@@ -59,6 +67,18 @@ rw contr_2 at h,
 by_contra h2,
 rw ← contr_1 at h2,
 show false, from h (i_conv (a_conv h2)),
+end
+
+lemma subaltrn_2_test : A e B → A o B :=       --   accidental conversion
+begin
+intro h,
+have h3 : c (A o B) → c (A e B) :=
+   begin
+      intro h4,
+      rw contr_e, rw contr_o at h4,
+      exact 
+   end,
+exact contr_5 h3 h,
 end
 
 /- Aristotle does not use 'extended' deductions (he does not use already proved theorems but only axioms)
@@ -91,6 +111,17 @@ rw contr_1 at h4,
 cc
 end
 
+lemma Baroco_test (h1: M a P) (h2 : M o S) : P o S :=
+begin
+have h3 : c (P o S) → c (M o S) := 
+   begin
+      intro h4,
+      rw contr_o at *,
+      exact Barbara₁ h1 h4,
+   end,
+exact contr_5 h3 h2,
+end
+
 --subaltern second-figure syllogisms that Aristotle did not mention
 lemma Cesaro₂ (h1: M e P) (h2 : M a S) : P o S :=
    subaltrn_2 (Celarent₁ (e_conv h1) h2)
@@ -113,6 +144,17 @@ rw ← contr_1 at h3,
 have h4 : P a M := Barbara₁ h3 h2,
 rw contr_1 at h4,
 cc
+end
+
+lemma Bocardo_test (h1 : P o M) (h2 : S a M) : P o S :=
+begin
+have h3 : c (P o S) → c (P o M) :=
+   begin
+      intro h4,
+      rw contr_o at *,
+      exact Barbara₁ h4 h2,
+   end,
+exact contr_5 h3 h1,
 end
 
 lemma Felapton₃ (h1 : P e M) (h2 : S a M) : P o S :=
@@ -156,6 +198,17 @@ rw contr_2 at h4,
 show false, from h4 h2
 end
 
+lemma Darii_test (h1 : P a M) (h2 : M i S) : P i S :=
+begin
+have h3 : c (P i S) → c (M i S) :=
+   begin
+      intro h4,
+      rw contr_i at *,
+      exact e_conv (Celarent₁ (e_conv h4) h1),
+   end,
+exact contr_5 h3 h2,
+end
+
 lemma Ferioₐ (h1 : P e M) (h2 : M i S) : P o S :=
 begin
 by_contra h3,
@@ -164,6 +217,17 @@ rw ← contr_1 at h3,
 have h4 : M e S :=  Cesare₂ h1 h3,
 rw contr_2 at h4,
 show false, from h4 h2
+end
+
+lemma Ferio_test (h1 : P e M) (h2 : M i S) : P o S :=
+begin
+have h3 : c (P o S) → c (M i S) :=
+   begin
+      intro h4,
+      rw contr_i, rw contr_o at h4,
+      exact Celarent₁ (e_conv h1) h4,
+   end,
+exact contr_5 h3 h2,
 end
 
 /- it is even possible to get rid of axiom i_conv -/
@@ -178,7 +242,19 @@ rw contr_2 at h3,
 cc
 end
 
-/- Aristotle also uses 'ecthesis' as an alternative proof method instead of proof by contradiction -/
--- ************** TODO ****************
+lemma i_conv_test : A i B → B i A :=
+begin
+intro h3,
+have h4 : c (B i A) → c (A i B) :=
+   begin
+      intro h5,
+      rw contr_i at *,
+      exact e_conv h5
+   end,
+exact contr_5 h4 h3,
+end
 
- 
+/- 
+************** TODO ****************
+prove that all invalid syllogisms cannot be derived
+-/
